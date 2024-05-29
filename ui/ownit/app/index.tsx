5a91ae103099ed26cea18297f971290feea7d123
shokouhSkr <shokouhskr@gmail.com>
2024-05-29 13:52:27 +0330

    preparation ui

---
+++ ui/ownit/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex-1 bg-purple-400 items-center justify-center">
+      <Text className="text-white text-3xl">Nativewind Rocks!✨</Text>
+    </View>
+  );
+};
+
+export default App;
