6abf51e3578f206b84fabe89847733f49b9182c6
shokouhSkr <shokouhskr@gmail.com>
2024-05-30 17:58:39 +0330

    add components folder

---
+++ ui/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View className="">
+      <Text className="">SearchBar</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
